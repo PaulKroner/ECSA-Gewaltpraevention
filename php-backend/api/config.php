7834cef05845fe2c9c4070c7a71550d5b9846ed7
Paul Kröner <paulfriedrich.kroener@gmail.com>
2025-02-17 10:10:49 +0100

    added .env config

--- php-backend/api/config.php
+++ php-backend/api/config.php
@@ -1,14 +1,22 @@
 <?php
-// Datenbankkonfiguration
-$host = "localhost"; // oder IP des Servers
-$dbname = "gewaltpraevention_edv";
-$username = "root"; // dein DB-Benutzer
-$password = ""; // dein DB-Passwort
+function loadEnv($path) {
+  if (!file_exists($path)) {
+      throw new Exception(".env-Datei nicht gefunden.");
+  }
+  $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+  foreach ($lines as $line) {
+      if (strpos(trim($line), '#') === 0) continue; // Kommentare ignorieren
+      list($name, $value) = explode('=', $line, 2);
+      putenv("$name=$value");
+  }
+}
+loadEnv(__DIR__ . '/.env');
 
-// header("Access-Control-Allow-Origin: http://localhost:3000");
-// header("Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS");
-// header("Access-Control-Allow-Headers: Content-Type, Authorization");
-// header("Access-Control-Allow-Credentials: true");
+// Datenbankkonfiguration
+$host = getenv('DB_HOST');
+$dbname = getenv('DB_NAME');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASS');
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password, [
