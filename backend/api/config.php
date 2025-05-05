a717c0a6c52385ee60999390f3f6d6880cafe3fc
Paul Kröner <paulfriedrich.kroener@gmail.com>
2025-05-05 17:28:39 +0200

    fix README info

--- backend/api/config.php
+++ backend/api/config.php
@@ -17,9 +17,10 @@ $host = getenv('DB_HOST');
 $dbname = getenv('DB_NAME');
 $username = getenv('DB_USER');
 $password = getenv('DB_PASS');
+$port = getenv('DB_PORT');
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password, [
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
     ]);
