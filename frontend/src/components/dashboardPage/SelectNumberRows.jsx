28e8c32d98dd4f2bed59328e2644e3c0609426b8
Paul Kröner <86837547+PaulKroner@users.noreply.github.com>
2025-07-07 13:50:20 +0200

    Update SelectNumberRows.jsx to 100 rows max

--- frontend/src/components/dashboardPage/SelectNumberRows.jsx
+++ frontend/src/components/dashboardPage/SelectNumberRows.jsx
@@ -19,7 +19,7 @@ const SelectNumberRows = ({ rowsPerPage, setRowsPerPage }) => {
           <SelectItem value="10">10</SelectItem>
           <SelectItem value="25">25</SelectItem>
           <SelectItem value="50">50</SelectItem>
-          <SelectItem value="500">500</SelectItem>
+          <SelectItem value="100">100</SelectItem>
         </SelectContent>
       </Select>
 
